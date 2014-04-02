1b0532c58cba70e95e72562522a6db17b58d89d7
Rakesh verma <rakesh.verma0487@gmail.com>
2014-04-02 09:50:05 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogAgain::Application.config.secret_key_base = '45841fc004ae298aed0888d1c586c6cfa6388cb300377aaf30c8c9645f5147e064feabe633a6b22d5486145eb69787f692265becc8607c7b7c9058fb59023c37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogAgain::Application.config.session_store :cookie_store, key: '_blog_again_session'
